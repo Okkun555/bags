1852e691389cc802dfa1d541fcc5335f13be2afa
Okkun555 <y.okuno.btm@gmail.com>
2026-08-01 13:14:10 +0900

    職業モデル、Factorybotを追加する

--- backend/app/models/occupation.rb
+++ backend/app/models/occupation.rb
@@ -1,4 +1,6 @@
 class Occupation < ApplicationRecord
+  has_many :profiles
+
   validates :name, uniqueness: true, length: { maximum: 100 }
   validates :sequence, uniqueness: true, numericality: { only_integer: true }
 end
--- backend/spec/factories/occupations.rb
+++ backend/spec/factories/occupations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :occupation do
-    
+    name { Faker::Job.title }
+    sequence(:sequence) { |n| n }
   end
 end
