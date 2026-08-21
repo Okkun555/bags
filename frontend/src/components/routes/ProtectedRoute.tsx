fca619fe0e66ad857794427e2ae179df543490f2
Okkun555 <y.okuno.btm@gmail.com>
2026-08-21 18:13:23 +0900

    プロフィール未作成の場合、プロフィール設定画面に遷移

--- frontend/src/components/routes/ProtectedRoute.tsx
+++ frontend/src/components/routes/ProtectedRoute.tsx
@@ -1,9 +1,10 @@
 import { useAuth } from "@/providers/AuthProvider";
 import { CircularProgress } from "@mui/material";
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 export const ProtectedRoute = () => {
   const { currentUser, isLoading } = useAuth();
+  const location = useLocation();
 
   if (isLoading) {
     return <CircularProgress />;
@@ -11,6 +12,10 @@ export const ProtectedRoute = () => {
 
   if (!currentUser || !currentUser.userId) {
     return <Navigate to="/login" replace />;
+  }
+
+  if (!currentUser.profile && location.pathname !== "/profile/new") {
+    return <Navigate to="/profile/new" replace />;
   }
 
   return <Outlet />;
